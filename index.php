82cd4777fa17e6896352635dd5c56f6384da846a
hidayabaikhati <109188713+hidayabaikhati@users.noreply.github.com>
2022-10-12 14:55:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<center>
+<body>
+    <h1>RUMAH SAKIT</h1>
+    <h1>KHAYANGAN</h1>
+</center>
+    <ul>
+        <li> <a href=index.php>Profil</a></li>
+        <li><a href=pasien.php> Lihat Pasien </a></li>
+        <li><a href=tambah_pasien.php> Tambah Pasien </a></li>
+        <li><a href=fasilitas.php> Fasilitas</a></li>
+</ul>
+</body>
+</html>
